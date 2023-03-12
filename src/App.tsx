caab88a6efc34ea3c18b352d502130f2e938e499
amanatsu027 <amanatsu027@gmail.com>
2023-03-12 11:25:01 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+// We will create these two pages in a moment
+import HomePage from "./pages/HomePage";
+import UserPage from "./pages/UserPage";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage></HomePage>} />
+      <Route path="/:id" element={<UserPage></UserPage>} />
+    </Routes>
   );
 }
-
-export default App;
